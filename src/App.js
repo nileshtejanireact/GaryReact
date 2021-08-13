2ed9b9f4e81febfc1af44df962ad9211bfc90545
nishantkinvints <nishantkinvints@gmail.com>
2021-08-13 09:41:15 +0530

    Latest2

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '../src/assets/css/style.css';
+import '../src/assets/css/new_style.css';
+import '../src/assets/css/responsive.css';
+
+
+
+//Main Directory Component
+import Header from '../src/Component/Header'
+import Siderbar from '../src/Component/Siderbar'
+import Appcontent from '../src/Component/Appcontent'
+
+
+import routes from './Routes';
+
+function App(props) {
+  return (
+    <>
+        <Header />
+        <Siderbar />
+        <Appcontent />
+    </>
+  );
+}
+
+export default App;
